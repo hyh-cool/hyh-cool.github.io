38458678612650e0d0178b3f75eed5105e00d66e
hyh-cool <hyh_cool@163.com>
2025-06-01 11:48:50 +0800

    TEST

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+export const SITE = {
+  website: "https://zxh.me/",
+  title: "Xiaohan Zou",
+  desc: "PhD student at Penn State",
+  author: "Xiaohan Zou"
+};
+
+export const THEME = {
+  editLink: {
+    pattern:
+      "https://github.com/Renovamen/renovamen.github.io/edit/main/src/content/blog:path",
+    text: "Edit this page on GitHub"
+  },
+  lastUpdatedText: "Last updated: "
+};
+
+export const GISCUS = {
+  repo: "This-is-an-Apple/giscus-zxh.io",
+  repoId: "R_kgDOGl2SjQ",
+  category: "Announcements",
+  categoryId: "DIC_kwDOGl2Sjc4CAcxK",
+  light: "light",
+  dark: `${SITE.website}/css/giscus-dark.css`
+};
